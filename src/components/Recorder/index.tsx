ac61a4c220b915f93a6d6a42658ffe76ac801026
Ted Stresen-Reuter <ted@secret-source.eu>
2022-09-30 22:49:34 +0100

    Removing the props param from the function signature

--- src/components/Recorder/index.tsx
+++ src/components/Recorder/index.tsx
@@ -4,7 +4,7 @@ import Visualizer from '../Visualizer'
 import './style.css'
 import useMediaRecorder from "../../hooks/useMediaRecorder";
 
-const Recorder = (props = {}) => {
+const Recorder = () => {
     const { recorder, recordings, setRecordings, isRecording, stream } = useMediaRecorder();
 
     const defaultRecordClass = 'record-play'
